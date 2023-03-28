18f59e7d7de85ec9d70044be6249cc80439e9437
Maarten Geerse <mgeerse@gmail.com>
2023-03-28 17:49:08 -0400

    chore: remove other outputs than text because BB breaks on other types

--- functions/array-loop/1.2/index.js
+++ functions/array-loop/1.2/index.js
@@ -3,10 +3,6 @@ const arrayLoop = async ({ input }, steps) => {
     throw new Error("Array Loop: The input isn't an Array");
   }
 
-  console.log('--- START ARRAY LOOP ---');
-  console.log(input);
-  console.log('--- END ARRAY LOOP ---');
-
   for (let index = 0; index < input.length; index += 1) {
     await steps({ iterator: input[index], index });
   }
